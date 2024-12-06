c1b3206a004ce02642c43c5804df60e2099daf52
enoch-bolarinwa <enoch-bolarinwa@>
2024-12-06 12:06:23 +0100

    foundation

---
+++ foundation/Video.cs
@@ -0,0 +1,32 @@
+using System;
+
+    class Video
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int DurationInSeconds { get; set; }
+        private List<Comment> Comments { get; set; }
+
+        public Video(string title, string author, int durationInSeconds)
+        {
+            Title = title;
+            Author = author;
+            DurationInSeconds = durationInSeconds;
+            Comments = new List<Comment>();
+        }
+
+        public void AddComment(Comment comment)
+        {
+            Comments.Add(comment);
+        }
+
+        public int GetCommentCount()
+        {
+            return Comments.Count;
+        }
+
+        public List<Comment> GetComments()
+        {
+            return Comments;
+        }
+    }
